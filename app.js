d560a50b32fad6ff32fe00cbdcb0f50fa1377381
enweremezechi <77008796+enweremezechi@users.noreply.github.com>
2021-05-21 21:09:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+convertFahrToCelsius();
+
+
+//declear the funcrion
+function convertFahrToCelsius(n){
+    const result = (n -32) *5/9;
+   for(var i=0; i<n.length;i++){
+       var str ='gsg';
+       if(n.length>1){
+           console.log(n + 'is not a valid number');
+       }
+       else if(n==='0||n==0'){
+           console.log('-17.7778')
+       }
+       else if(n===str){
+           console.log(n + 'is not a string');
+       }
+    }
+ }
+ convertFahrToCelsius();
+
